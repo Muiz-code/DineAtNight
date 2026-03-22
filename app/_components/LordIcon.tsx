ae4293d1d847af12ff3c3b70b8f4d3e218361a85
muiz-code <hollarewajumuiz@gmail.com>
2026-03-23 00:39:37 +0100

    Remove global JSX declaration for LordIcon component in LordIcon.tsx to streamline code structure.

--- app/_components/LordIcon.tsx
+++ app/_components/LordIcon.tsx
@@ -1,20 +1,5 @@
 "use client";
 
-declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      "lord-icon": React.DetailedHTMLProps<
-        React.HTMLAttributes<HTMLElement>,
-        HTMLElement
-      > & {
-        src?: string;
-        trigger?: string;
-        colors?: string;
-      };
-    }
-  }
-}
-
 interface LordIconProps {
   src?: string;
   trigger?: string;
